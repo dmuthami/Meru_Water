f484830653cbff8ce143cdd56204c03b69e4a619
David Muthami <waruid@gmail.com>
2012-10-17 01:53:27 +0300

    Version 1.0.0.0

---
+++ MW/frmMain.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace MW
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tpgMap = new System.Windows.Forms.TabPage();
+            this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
+            this.axToolbarControl1 = new ESRI.ArcGIS.Controls.AxToolbarControl();
+            this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
+            this.axTOCControl1 = new ESRI.ArcGIS.Controls.AxTOCControl();
+            this.tpgAttributeTable = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuNewDoc = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuOpenDoc = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuSaveDoc = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuExitApp = new System.Windows.Forms.ToolStripMenuItem();
+            this.securityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusBarXY = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabControl1.SuspendLayout();
+            this.tpgMap.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tpgMap);
+            this.tabControl1.Controls.Add(this.tpgAttributeTable);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(586, 484);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tpgMap
+            // 
+            this.tpgMap.Controls.Add(this.axLicenseControl1);
+            this.tpgMap.Controls.Add(this.axToolbarControl1);
+            this.tpgMap.Controls.Add(this.axMapControl1);
+            this.tpgMap.Controls.Add(this.axTOCControl1);
+            this.tpgMap.Location = new System.Drawing.Point(4, 22);
+            this.tpgMap.Name = "tpgMap";
+            this.tpgMap.Padding = new System.Windows.Forms.Padding(3);
+            this.tpgMap.Size = new System.Drawing.Size(578, 458);
+            this.tpgMap.TabIndex = 0;
+            this.tpgMap.Text = "Map";
+            this.tpgMap.UseVisualStyleBackColor = true;
+            // 
+            // axLicenseControl1
+            // 
+            this.axLicenseControl1.Enabled = true;
+            this.axLicenseControl1.Location = new System.Drawing.Point(156, 200);
+            this.axLicenseControl1.Name = "axLicenseControl1";
+            this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
+            this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl1.TabIndex = 3;
+            // 
+            // axToolbarControl1
+            // 
+            this.axToolbarControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.axToolbarControl1.Location = new System.Drawing.Point(3, 3);
+            this.axToolbarControl1.Name = "axToolbarControl1";
+            this.axToolbarControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axToolbarControl1.OcxState")));
+            this.axToolbarControl1.Size = new System.Drawing.Size(572, 28);
+            this.axToolbarControl1.TabIndex = 2;
+            // 
+            // axMapControl1
+            // 
+            this.axMapControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axMapControl1.Location = new System.Drawing.Point(156, 37);
+            this.axMapControl1.Name = "axMapControl1";
+            this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
+            this.axMapControl1.Size = new System.Drawing.Size(419, 418);
+            this.axMapControl1.TabIndex = 1;
+            this.axMapControl1.OnMouseMove += new ESRI.ArcGIS.Controls.IMapControlEvents2_Ax_OnMouseMoveEventHandler(this.axMapControl1_OnMouseMove);
+            this.axMapControl1.OnMapReplaced += new ESRI.ArcGIS.Controls.IMapControlEvents2_Ax_OnMapReplacedEventHandler(this.axMapControl1_OnMapReplaced);
+            // 
+            // axTOCControl1
+            // 
+            this.axTOCControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.axTOCControl1.Location = new System.Drawing.Point(3, 37);
+            this.axTOCControl1.Name = "axTOCControl1";
+            this.axTOCControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTOCControl1.OcxState")));
+            this.axTOCControl1.Size = new System.Drawing.Size(151, 418);
+            this.axTOCControl1.TabIndex = 0;
+            // 
+            // tpgAttributeTable
+            // 
+            this.tpgAttributeTable.Location = new System.Drawing.Point(4, 22);
+            this.tpgAttributeTable.Name = "tpgAttributeTable";
+            this.tpgAttributeTable.Padding = new System.Windows.Forms.Padding(3);
+            this.tpgAttributeTable.Size = new System.Drawing.Size(578, 458);
+            this.tpgAttributeTable.TabIndex = 1;
+            this.tpgAttributeTable.Text = "Attribute Tables";
+            this.tpgAttributeTable.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.securityToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(586, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuNewDoc,
+            this.menuOpenDoc,
+            this.menuSaveDoc,
+            this.menuSaveAs,
+            this.menuExitApp});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // menuNewDoc
+            // 
+            this.menuNewDoc.Image = ((System.Drawing.Image)(resources.GetObject("menuNewDoc.Image")));
+            this.menuNewDoc.Name = "menuNewDoc";
+            this.menuNewDoc.Size = new System.Drawing.Size(152, 22);
+            this.menuNewDoc.Text = "New Document";
+            this.menuNewDoc.Click += new System.EventHandler(this.menuNewDoc_Click);
+            // 
+            // menuOpenDoc
+            // 
+            this.menuOpenDoc.Image = ((System.Drawing.Image)(resources.GetObject("menuOpenDoc.Image")));
+            this.menuOpenDoc.Name = "menuOpenDoc";
+            this.menuOpenDoc.Size = new System.Drawing.Size(152, 22);
+            this.menuOpenDoc.Text = "Open Document";
+            this.menuOpenDoc.Click += new System.EventHandler(this.menuOpenDoc_Click);
+            // 
+            // menuSaveDoc
+            // 
+            this.menuSaveDoc.Image = ((System.Drawing.Image)(resources.GetObject("menuSaveDoc.Image")));
+            this.menuSaveDoc.Name = "menuSaveDoc";
+            this.menuSaveDoc.Size = new System.Drawing.Size(152, 22);
+            this.menuSaveDoc.Text = "Save Document";
+            this.menuSaveDoc.Click += new System.EventHandler(this.menuSaveDoc_Click);
+            // 
+            // menuSaveAs
+            // 
+            this.menuSaveAs.Image = ((System.Drawing.Image)(resources.GetObject("menuSaveAs.Image")));
+            this.menuSaveAs.Name = "menuSaveAs";
+            this.menuSaveAs.Size = new System.Drawing.Size(152, 22);
+            this.menuSaveAs.Text = "Save As...";
+            this.menuSaveAs.Click += new System.EventHandler(this.menuSaveAs_Click);
+            // 
+            // menuExitApp
+            // 
+            this.menuExitApp.Name = "menuExitApp";
+            this.menuExitApp.Size = new System.Drawing.Size(152, 22);
+            this.menuExitApp.Text = "Exit";
+            this.menuExitApp.Click += new System.EventHandler(this.menuExitApp_Click);
+            // 
+            // securityToolStripMenuItem
+            // 
+            this.securityToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loginInToolStripMenuItem,
+            this.logOutToolStripMenuItem});
+            this.securityToolStripMenuItem.Name = "securityToolStripMenuItem";
+            this.securityToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.securityToolStripMenuItem.Text = "Security";
+            // 
+            // loginInToolStripMenuItem
+            // 
+            this.loginInToolStripMenuItem.Name = "loginInToolStripMenuItem";
+            this.loginInToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.loginInToolStripMenuItem.Text = "Login In";
+            // 
+            // logOutToolStripMenuItem
+            // 
+            this.logOutToolStripMenuItem.Name = "logOutToolStripMenuItem";
+            this.logOutToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.logOutToolStripMenuItem.Text = "Log Out";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusBarXY});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 486);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(586, 22);
+            this.statusStrip1.Stretch = false;
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusBar1";
+            // 
+            // statusBarXY
+            // 
+            this.statusBarXY.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenOuter;
+            this.statusBarXY.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.statusBarXY.Name = "statusBarXY";
+            this.statusBarXY.Size = new System.Drawing.Size(49, 17);
+            this.statusBarXY.Text = "Test 123";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 508);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "Meru Water";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tpgMap.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tpgMap;
+        private System.Windows.Forms.TabPage tpgAttributeTable;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuNewDoc;
+        private System.Windows.Forms.ToolStripMenuItem securityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logOutToolStripMenuItem;
+        private ESRI.ArcGIS.Controls.AxToolbarControl axToolbarControl1;
+        private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
+        private ESRI.ArcGIS.Controls.AxTOCControl axTOCControl1;
+        private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusBarXY;
+        private System.Windows.Forms.ToolStripMenuItem menuOpenDoc;
+        private System.Windows.Forms.ToolStripMenuItem menuSaveDoc;
+        private System.Windows.Forms.ToolStripMenuItem menuSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem menuExitApp;
+    }
+}
+
---
+++ MW/frmMain.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.SystemUI;
+using ESRI.ArcGIS.Carto;
+
+
+namespace MW
+{
+    public partial class frmMain : Form
+    {
+        #region class private members
+        private IMapControl3 m_mapControl = null;
+        private string m_mapDocumentName = string.Empty;
+        #endregion
+
+        #region class constructor
+         public frmMain()
+        {
+            InitializeComponent();
+        }       
+        #endregion
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Main Menu event handlers
+
+        private void menuNewDoc_Click(object sender, EventArgs e)
+        {
+            //execute New Document command
+            
+            ICommand command = new  CreateNewDocument();
+            command.OnCreate(m_mapControl.Object);
+            command.OnClick();
+        }
+
+        private void menuOpenDoc_Click(object sender, EventArgs e)
+        {
+            //execute Open Document command
+            ICommand command = new ControlsOpenDocCommandClass();
+            command.OnCreate(m_mapControl.Object);
+            command.OnClick();
+        }
+
+        private void menuSaveDoc_Click(object sender, EventArgs e)
+        {
+            //execute Save Document command
+            if (m_mapControl.CheckMxFile(m_mapDocumentName))
+            {
+                //create a new instance of a MapDocument
+                IMapDocument mapDoc = new MapDocumentClass();
+                mapDoc.Open(m_mapDocumentName, string.Empty);
+
+                //Make sure that the MapDocument is not readonly
+                if (mapDoc.get_IsReadOnly(m_mapDocumentName))
+                {
+                    MessageBox.Show("Map document is read only!");
+                    mapDoc.Close();
+                    return;
+                }
+
+                //Replace its contents with the current map
+                mapDoc.ReplaceContents((IMxdContents)m_mapControl.Map);
+
+                //save the MapDocument in order to persist it
+                mapDoc.Save(mapDoc.UsesRelativePaths, false);
+
+                //close the MapDocument
+                mapDoc.Close();
+            }
+        }
+
+        private void menuSaveAs_Click(object sender, EventArgs e)
+        {
+            //execute SaveAs Document command
+            ICommand command = new ControlsSaveAsDocCommandClass();
+            command.OnCreate(m_mapControl.Object);
+            command.OnClick();
+        }
+
+        private void menuExitApp_Click(object sender, EventArgs e)
+        {
+            //exit the application
+            Application.Exit();
+        }
+   
+        #endregion
+
+        private void axMapControl1_OnMapReplaced(object sender, ESRI.ArcGIS.Controls.IMapControlEvents2_OnMapReplacedEvent e)
+        {
+
+            //get the current document name from the MapControl
+            m_mapDocumentName = m_mapControl.DocumentFilename;
+
+            //if there is no MapDocument, diable the Save menu and clear the statusbar
+            if (m_mapDocumentName == string.Empty)
+            {
+                menuSaveDoc.Enabled = false;
+                statusBarXY.Text = string.Empty;
+            }
+            else
+            {
+                //enable the Save manu and write the doc name to the statusbar
+                menuSaveDoc.Enabled = true;
+                statusBarXY.Text = Path.GetFileName(m_mapDocumentName);
+            }
+
+        }
+
+        private void axMapControl1_OnMouseMove(object sender, ESRI.ArcGIS.Controls.IMapControlEvents2_OnMouseMoveEvent e)
+        {
+            statusBarXY.Text = string.Format("{0}, {1}  {2}", e.mapX.ToString("#######.##"), e.mapY.ToString("#######.##"), axMapControl1.MapUnits.ToString().Substring(4));
+        }
+
+
+    }
+}
